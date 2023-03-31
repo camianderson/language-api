2ba7345efb2db23d405bac1c47ee9ff46ec56879
Cami Anderson <camidantas@icloud.com>
2023-03-31 11:19:35 -0400

    Add Post method to the API

--- src/main/java/br/com/alura/languages/api/LanguageController.java
+++ src/main/java/br/com/alura/languages/api/LanguageController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +18,11 @@ public class LanguageController {
     public List<Language> obtainLanguages() {
         List<Language> languages = repository.findAll();
         return languages;
+    }
+
+    @PostMapping ("/languages")
+    public Language addLanguage(@RequestBody Language language) {
+        Language savedLanguage = repository.save(language);
+        return savedLanguage;
     }
 }
