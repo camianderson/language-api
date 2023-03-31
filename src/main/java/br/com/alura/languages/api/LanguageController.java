b40902a20d16a9aa5dcd9ca81c10a4d57501849b
Cami Anderson <camidantas@icloud.com>
2023-03-31 09:32:54 -0400

    Add first list of languages to the API

--- src/main/java/br/com/alura/languages/api/LanguageController.java
+++ src/main/java/br/com/alura/languages/api/LanguageController.java
@@ -1,13 +1,21 @@
 package br.com.alura.languages.api;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LanguageController {
 
-    @GetMapping(value="/linguagem-preferida")
-    public String processaLinguagePreferida(){
-        return "Oi, Java!";
+    private List<Language> languages = 
+        List.of(
+            new Language("Java", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/java/java_256x256.png", 1),
+            new Language("PHP", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/php/php_256x256.png", 2)
+        );
+
+    @GetMapping ("/languages")
+    public List<Language> obtainLanguages() {
+        return languages;
     }
 }
